9be54f859e5dc6a29a4b27e7be242560e738a64e
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 20:59:57 -0300

    logical: desafio 10

---
+++ src/desafio10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio10
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("inicial");
+            var initial = int.Parse(Console.ReadLine());
+            Console.WriteLine("Final");
+            var final = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(diffHours(initial, final));
+        }
+        private static int diffHours(int hourInitial, int hourFinal)
+        {
+            return hourInitial < hourFinal ? hourFinal - hourInitial : 24 - hourInitial + hourFinal;
+        }
+    }
+}
+
