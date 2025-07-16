fcff11cafbc4c3cfc89b3779a6ce1ed4d97afa0c
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 20:59:51 -0300

    logical: desafio 09

---
+++ src/desafio09/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Desafio09;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        int x, y;
+        Console.WriteLine("X: ");
+        x = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Y: ");
+        y = int.Parse(Console.ReadLine());
+        
+        var result = y % x == 0 || x % y == 0 ? "Multiplo" : "Não Multiplo";
+        Console.WriteLine(result);
+        
+    }
+}
