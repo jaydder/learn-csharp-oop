3db9400b807e39fbb39d87e7159fbd235c9242b3
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 08:53:25 -0300

    logical: desafio 07

---
+++ src/desafio07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIgite a bagaça");
+            int value = int.Parse(Console.ReadLine());
+
+
+            String result = value < 0 ? "Negativo" : "Positivo";
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
