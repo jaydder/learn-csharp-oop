3143294226aba30cfc33c2825632e4c297804937
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 07:04:10 -0300

    logical: desafio 02

---
+++ src/desafio02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 100.64;
+            double area = Area(x);
+            Console.WriteLine(area);
+
+        }
+
+        static double Area(double diametro) 
+        {
+            double raio = diametro / 2;
+            return Math.Round(3.14159 * Math.Pow(raio,2), 4);
+        }
+    }
+}
