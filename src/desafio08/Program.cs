bc7748f40a987c4c41e594551870673d37d09582
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 20:59:41 -0300

    logical: desafio 08

---
+++ src/desafio08/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Desafio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor");
+            int number =  int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(isOdd(number));
+            
+            
+        }
+
+        static string isOdd(int number)
+        {
+           return number % 2 == 0 ? "even" : "odd"; 
+        }
+    }
+}
+
