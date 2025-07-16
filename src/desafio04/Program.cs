3531c02df848216556a277243e9af5b618dd23b4
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 07:32:48 -0300

    logical: desafio 04

---
+++ src/desafio04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employee, workHours;
+            double valueHour;
+
+            employee = 6;
+            workHours = 145;
+            valueHour = 15.55;
+
+            double difference = salary(workHours, valueHour);
+
+            Console.WriteLine("Employe: "+ employee);
+            Console.WriteLine("Salary: "+ difference);
+        }
+
+        static double salary(int workHours, double valueHour)
+        {
+            return Math.Round(workHours * valueHour, 2);
+        }
+    } 
+}
