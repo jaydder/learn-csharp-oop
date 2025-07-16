00f9f708466a0faad9348eec8d7bddff62a7bdc0
jaydder <jaydderfavoravel@gmail.com>
2025-07-16 08:20:31 -0300

    logical: desafio 05

---
+++ src/desafio05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace Desafio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int code, quantity;
+            double value, total1, total2;
+
+            code = 13;
+            quantity = 2;
+            value = 15.30;
+
+            total1 = resume(quantity, value);
+
+            code = 161;
+            quantity = 4;
+            value = 5.20;
+
+
+            total2 = resume(quantity, value);
+
+            Console.WriteLine("Total:" + Math.Round(total1 + total2, 2));
+
+        }
+
+        static double resume(int quantity, double value)
+        {
+            return Math.Round(quantity * value, 2);
+        
+        }
+    }
+}
