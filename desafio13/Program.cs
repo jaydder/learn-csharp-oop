367a1c79ef1cf83631b82e8cc4794dc7084fc012
jaydder <jaydderfavoravel@gmail.com>
2025-07-17 09:52:58 -0300

    logical: desafio13

---
+++ desafio13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Desafio13
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      double x, y;
+      
+      x = 4.5;
+      y = -2.2;
+      Console.WriteLine(Coordenate(x,y));
+      
+      x = 0.1;
+      y = 0.1;
+      Console.WriteLine(Coordenate(x,y));
+      
+      x = 0.0;
+      y = -0.0;
+      Console.WriteLine(Coordenate(x,y));
+
+    }
+
+    private static string Coordenate(double x, double y) 
+    {
+      
+      if (x > 0 && y > 0)
+      {
+        return "Q1";
+      } 
+      else if (x < 0 && y > 0)
+      {
+        return "Q2";
+      }
+      
+      else if (x < 0 && y > 0)
+      {
+        return "Q3";
+      }
+      else if (x > 0 && y < 0)
+      {
+        return "Q4";
+      }
+      else
+      {
+        return "Origem";
+      }
+    }
+  }
+}
