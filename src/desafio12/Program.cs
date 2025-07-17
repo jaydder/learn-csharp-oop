1f3d9ca377f1aac43a2ec157bd9c6899cbed1de9
jaydder <jaydderfavoravel@gmail.com>
2025-07-17 07:42:54 -0300

    logical: desafio 12

--- src/desafio12/Program.cs
+++ src/desafio12/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualBasic;
 
-Console.WriteLine("Hello, World!");
+namespace Desafio12
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      Console.WriteLine("Digita a bagaça");
+      double value = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture );
+      
+      Console.WriteLine(Interval(value));
+      
+    }
+
+    private static string Interval(double number)
+    {
+      Console.Write(number);
+      return number switch
+      {
+        < 0.00 or > 100.00 => "Fora do Escopo",
+        <= 25.00 => "Intervalo [0,25]",
+        <= 50.00 => "Intervalo (25,50]",
+        <= 75.00 => "Intervalo (50,75]",
+        _ => "Intervalo (75,100]"
+      };
+    }
+  }
+}
+
