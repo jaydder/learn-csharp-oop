6c49518fa32822fb076a4558d9d3cfbe6bc78775
jaydder <jaydderfavoravel@gmail.com>
2025-07-18 01:45:56 -0300

    logical: desafio 22

---
+++ src/desafio22/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Desafio22
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("numero: ");
+            int number = int.Parse(Console.ReadLine());
+            var result = 1;
+            
+            for(int i = 1; i <= number; i ++)
+            {
+               result *= i; 
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
