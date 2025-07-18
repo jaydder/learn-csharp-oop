f9ece56d5541b1cface4b0b3daba581715f8df65
jaydder <jaydderfavoravel@gmail.com>
2025-07-18 02:45:26 -0300

    logical: desafio 24

---
+++ src/desafio24/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Desafio24
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("digita a bagaça");
+            var number = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= number; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+            }
+        }
+    }
+}
