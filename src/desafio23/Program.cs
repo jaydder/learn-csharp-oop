6c0e48f3088e959de2c3ed99ff8e757d4e360db1
jaydder <jaydderfavoravel@gmail.com>
2025-07-18 02:45:21 -0300

    logical: desafio 23

---
+++ src/desafio23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Desafio23
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("digita a bagaça");
+            var number = int.Parse(Console.ReadLine());
+
+            for (var i = number; i != 0; i--)
+            {
+                if (number % i == 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
