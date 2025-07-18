d9fa2fef2143c372f802d5a737cc951af9cf3517
jaydder <jaydderfavoravel@gmail.com>
2025-07-18 01:45:51 -0300

    logical: desafio 21

---
+++ src/desafio21/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Desafio21
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("digita valor: ");
+            int value = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < value; i++)
+            {
+                
+                string[] numbers = Console.ReadLine().Split(' ');
+                double number = double.Parse(numbers[0]);
+                double number2 = double.Parse(numbers[1]);
+
+                if (number2 == 0)
+                {
+                    Console.WriteLine("Divisão por zero");
+                }
+                else
+                {
+                    Console.WriteLine(number / number2);
+                }
+                
+
+            }
+        }
+    }
+}
