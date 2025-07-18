2da0acbfe8190fa9b3c123dae5a147dbcf44a4e6
jaydder <jaydderfavoravel@gmail.com>
2025-07-18 01:45:12 -0300

    logical: desafio 18

---
+++ src/desafio18/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Desafio18
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite 1 e 1000");
+            var number =  int.Parse(Console.ReadLine());
+
+            for (var i = 0; i <= number; i++)
+            {
+                if (i % 2 != 0) Console.Write($"\nvalor impar {i}");
+            }
+        }
+    }
+}
