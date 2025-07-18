cbbe344fd0e67adf2ad4e47db2e7db439fb54d8e
jaydder <jaydderfavoravel@gmail.com>
2025-07-17 22:45:24 -0300

    logical: desafio15

---
+++ src/desafio15/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Desafio15
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+
+      while (true)
+      {
+        Console.WriteLine("Digite a senha");
+        int pwd = int.Parse(Console.ReadLine());
+        
+        if (pwd == 2002)
+        {
+          Console.WriteLine("Senha valida");
+          break;
+        }
+        else
+        {
+          Console.WriteLine("Senha invalida");
+        }
+      }
+    }
+
+  }
+}
+
